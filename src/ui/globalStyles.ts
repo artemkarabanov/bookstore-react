365bb44666ff4cca24643c6d0583a36cd79c5cd6
artemkarabanov <113052815+artemkarabanov@users.noreply.github.com>
2023-05-01 22:20:30 +0300

    init: bookstore

---
+++ src/ui/globalStyles.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  ${onreset}
+  body {
+    font-family: 'Exo 2', sans-serif;
+    font-style: normal
+  }
+`;
